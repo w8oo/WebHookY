d311da84fd39208530f9acdba86df5a9563f8faa
w8oo <113918643+w8oo@users.noreply.github.com>
2022-12-21 00:08:49 +0300

    forget to upload it lol

---
+++ main.py
@@ -0,0 +1,31 @@
+import requests
+
+WEBHOOK_URL = "https://discord.com/api/webhooks/1025382145511927849/8fBC6kaVRn0UbtrB-xdhnBnl_9N6pnAEYrf-Kd4-NqjLtMXrMeOFQh37YUeiDuyL4-3Y"
+
+response = requests.get(WEBHOOK_URL)
+
+if response.status_code == 200:
+    webhook_info = response.json()
+
+    with open("webhook_info.txt", "w") as f:
+        f.write(str(webhook_info))
+
+    delete_response = requests.delete(WEBHOOK_URL)
+
+    if delete_response.status_code == 204:
+        print("Webhook deleted successfully")
+    else:
+        print("Error deleting webhook")
+else:
+    print("Error getting webhook information")
+
+data = {
+    "content": "This is a test message"
+}
+send_response = requests.post(WEBHOOK_URL, json=data)
+
+if send_response.status_code == 404:
+
+    print("Message not sent - webhook does not exist")
+else:
+    print("Error sending message")
